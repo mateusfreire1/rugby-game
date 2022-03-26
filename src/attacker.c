4905daac08ff9b2cd0d1ba48590c7de0dfc781ca
unknown <mateus.freire@usp.br>
2022-03-26 20:59:12 -0300

    Implement wacky attacker and defender strategy

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -18,12 +20,50 @@
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  static int turn_count = 0; //Variable to keep track of turn count.
+  position_t x = attacker_position;
+  srand(time(NULL));
+
+  if (get_spy_number_uses(defender_spy) < 1 && turn_count == 3){
+    x = get_spy_position(defender_spy); //Perform spying.
+  }
+  
+  if (turn_count < 3){            //For the first three turns, move upwards.
+    turn_count++;
+    return (direction_t) DIR_UP;
+  }
+  else{                           
+    if (x.j > attacker_position.j && (abs(x.j)-abs(x.j)) >= 2){       //If defender is below and far away, move right.
+      turn_count++;
+      return (direction_t) DIR_RIGHT;
+    }
+    else if (x.j > attacker_position.j && (abs(x.j)-abs(x.j)) < 2){   //If defender is below but not far, move up again.
+      turn_count++;
+      return (direction_t) DIR_UP;
+    }
+    else if (x.j < attacker_position.j && (abs(x.j)-abs(x.j)) < 2){   //If defender is above, move down.
+      turn_count++;
+      return (direction_t) DIR_DOWN;
+    }
+    else{     //Main movement logic, where the attacker always tends to move right, however with a chance to deviate paths.
+      turn_count++;
+      int fate = rand() % 100 + 1;
+      if (fate > 90)
+        return (direction_t) DIR_UP;
+      else if (fate > 50 && fate <= 90)
+        return (direction_t) DIR_RIGHT;
+      else if (fate > 30 && fate <= 50)
+        return (direction_t) DIR_UP_RIGHT;  
+      else if (fate > 10 && fate <= 30)
+        return (direction_t) DIR_DOWN_RIGHT;
+      else
+        return (direction_t) DIR_DOWN;
+    }
+  }
+  
+
+  
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -18,11 +20,66 @@
 
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
+  static int turn_count = 0;  //Variable to keep track of turn count.
+  static int tendency = 0;    //Variable to keep track of the attacker's approximate direction.
+  position_t x = defender_position;
+  srand(time(NULL));
+
+  if (get_spy_number_uses(attacker_spy) < 1 && turn_count == 2){
+    x = get_spy_position(attacker_spy);    //Perform spying.
+    if (x.j > defender_position.j){        //If attacker is below defender, move down and restart turn cycle.
+      turn_count = 4;
+      tendency = 1;
+      return (direction_t) DIR_DOWN;
+    }
+    else if (x.j < defender_position.j){   //If attacker is above defender, move up and restart turn cycle.
+      turn_count = 4;
+      tendency = -1;
+      return (direction_t) DIR_UP;
+    }
+    else {                                //If attacker is at the same height as defender, stay and restart turn cycle.
+      turn_count = 4;
+      tendency = 0;
+      return (direction_t) DIR_STAY;
+    }
+  }
+
+  //Create a cycle of moves that repeats periodically, with a chance to deviate based on the position recieved on the spy.
+  int turn_cycle = turn_count % 4;
+  int fate = rand() % 100 + 1;
+  if (fate > 90){
+    switch (tendency){
+    case -1:
+        turn_count++;
+        return (direction_t) DIR_UP;
+    case 0:
+        turn_count++;
+        return (direction_t) DIR_STAY;
+    case 1:
+        turn_count++;
+        return (direction_t) DIR_DOWN;
+    }
+  }
+  else{
+    switch(turn_cycle){
+      case 0:
+          turn_count++;
+          return (direction_t) DIR_UP_LEFT;
+      case 1:
+          turn_count++;
+          return (direction_t) DIR_DOWN_RIGHT;
+      case 2:
+          turn_count++;
+          return (direction_t) DIR_DOWN_LEFT;
+      case 3:
+          turn_count++;
+          return (direction_t) DIR_UP_RIGHT;
+    }
+
+  }
+  
+  
   return (direction_t) DIR_LEFT;
 }
 
